2e6dcbd811a9307b616600d6ad37d8bbbd24610d
Ellis Spawforth <94527156+espawforth01@users.noreply.github.com>
2022-12-12 13:58:08 +0000

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,21 @@
+const phoneList = [
+    {name:"Pixel 6a", brand:"Google", memory:"128 GB", color:"white"&&"green"&&"black", os:"Android", cost:399},
+    {name:"Pixel 6 Pro", brand:"Google", memory:"256 GB", color:"black", os:"Android", cost:749},
+    {name:"iPhone 12 Pro", brand:"Apple", memory:"512 GB", color:"gold"&&"gray"&&"silver", os:"iOS", cost:1149},
+    {name:"iPhone SE 2020", brand:"Apple", memory:"64 GB", color:"red"&&"white", os:"iOS", cost:349},
+    {name:"iPhone 13 Mini", brand:"Apple", memory:"256 GB", color:"blue"&&"white"&&"black"&&"pink", os:"iOS", cost:729},
+    {name:"G82", brand:"Motorola", memory:"128 GB", color:"gray", os:"Android", cost:249},
+    {name:"Moto E30", brand:"Motorola", memory:"32 GB", color:"blue"&&"gray", os:"Android", cost:89},
+    {name:"Galaxy S22 Plus", brand:"Samsung", memory:"128 GB", color:"green"&&"black"&&"white"&&"pink", os:"Android", cost:949},
+    {name:"Galaxy A13", brand:"Samsung", memory:"64 GB", color:"white"&&"black", os:"Android", cost:129},
+    {name:"105", brand:"Nokia", memory:"4 MB", color:"black"&&"blue"&&"pink", os:"Symbian OS", cost:25},
+];
+
+//search functions
+
+
+//displayContent function
+
+
+//onLoad function
+
